9c3b323ce83945ea6c9c67b0048ce0d88a6bb802
Azazel 00 <pfbhjdfhn@mail.ru>
2023-09-15 21:30:37 +0500

    Task4

---
+++ Tyuiu.ZairovAR.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZairovAR.Sprint0.Task4.V0.Lib;
+
+
+namespace Tyuiu.ZairovAR.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(1, 5));
+            Console.WriteLine(DataService.Division(9, 3));
+            Console.WriteLine(DataService.Multiplication(1, 5));
+            Console.ReadKey();
+        }
+    }
+}
